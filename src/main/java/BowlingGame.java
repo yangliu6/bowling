9357c1d828bd0a2769c6ecb0922da1ffc7784f76
yangliu <1695938413@qq.com>
2020-06-01 17:58:08 +0800

    If tenth game STRIKE,the score should be 10 plus the following two ball strike bottles

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -17,6 +17,14 @@ public class BowlingGame {
         bowlingScore.add(singleBowlingScore);
     }
 
+    public void throwBowling(Bowling bowling1, Bowling bowling2, Bowling bowling3){
+        List<Integer> singleBowlingScore = new ArrayList<>();
+        singleBowlingScore.add(bowling1.getScore());
+        singleBowlingScore.add(bowling2.getScore());
+        singleBowlingScore.add(bowling3.getScore());
+        bowlingScore.add(singleBowlingScore);
+    }
+
     public int calculateSingleGameScore(int times) {
         //times 表示第几次
         int score = 0;
@@ -25,14 +33,19 @@ public class BowlingGame {
             currentScore += (int)bowlingScore.get(times-1).get(i);
         }
 
-        if((int)bowlingScore.get(times-1).get(0) == 10 && (int)bowlingScore.get(times).get(0) != 10){
-            score = 10 +  (int)bowlingScore.get(times).get(0)+ (int) bowlingScore.get(times).get(1);
+        if(times != 10){
+            if((int)bowlingScore.get(times-1).get(0) == 10 && (int)bowlingScore.get(times).get(0) != 10){
+                score = 10 +  (int)bowlingScore.get(times).get(0)+ (int) bowlingScore.get(times).get(1);
+            }
+            else if((int)bowlingScore.get(times-1).get(0) == 10 && (int)bowlingScore.get(times).get(0) == 10){
+                score = 20 + (int)bowlingScore.get(times+1).get(0);
+            }
+            else if(currentScore == 10 && times != 10){
+                score = 10 + (int)bowlingScore.get(times).get(0);
+            }
         }
-        else if((int)bowlingScore.get(times-1).get(0) == 10 && (int)bowlingScore.get(times).get(0) == 10){
-            score = 20 + (int)bowlingScore.get(times+1).get(0);
-        }
-        else if(currentScore == 10){
-            score = 10 + (int)bowlingScore.get(times).get(0);
+        else {
+            score = currentScore;
         }
         return score;
     }
