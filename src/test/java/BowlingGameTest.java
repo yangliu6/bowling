ee5eac038e0484aba9f10d147c007f4a0603eee0
yangliu <1695938413@qq.com>
2020-06-01 16:52:48 +0800

    If first game STRIKE and second one not STRIKE,the score of first game should be 10 plus following two balls strike bottle

---
+++ src/test/java/BowlingGameTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import static junit.framework.TestCase.assertEquals;
+
+
+public class BowlingGameTest {
+    @Test
+    public void should_be_10_plus_following_two_balls_strike_bottles_when_calculate_first_game_score_given_first_game_STRIKE_and_second_game_not_STRIKE(){
+        BowlingGame bowlingGame = new BowlingGame();
+        Bowling bowling1 = new Bowling();
+        bowling1.setScore(10);
+        Bowling bowling2 = new Bowling();
+        bowling2.setScore(5);
+        bowlingGame.throwBowling(bowling1,null);
+        bowlingGame.throwBowling(bowling2,bowling2);
+
+        int score = bowlingGame.calculateSingleGameScore(1);
+
+        assertEquals(20,score);
+    }
+}
